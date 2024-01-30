ea645e4e1a998801c8dbc7a7075f6bc0b899a3cf
tbohne <tim.bohne@dfki.de>
2024-01-30 11:07:47 +0100

    implement setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+__version__ = '0.1.1'
+URL = 'https://github.com/tbohne/nesy_diag_smach'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+for i in range(len(required)):
+    # adapt the repo references for setup.py usage
+    if "https" in required[i]:
+        pkg = required[i].split(".git")[0].split("/")[-1]
+        required[i] = pkg + "@" + required[i]
+
+setup(
+    name='nesy_diag_smach',
+    version=__version__,
+    description='Neuro-symbolic anomaly detection and complex fault diagnosis.',
+    author='Tim Bohne',
+    author_email='tim.bohne@dfki.de',
+    url=URL,
+    download_url=f'{URL}/archive/{__version__}.tar.gz',
+    keywords=[
+        'neural-networks',
+        'knowledge-graphs',
+        'neuro-symbolic-ai',
+        'state-machine',
+        'anomaly-detection',
+        'fault-diagnosis'
+    ],
+    python_requires='>=3.7, <3.11',
+    install_requires=required,
+    packages=find_packages(),
+    include_package_data=True,
+)

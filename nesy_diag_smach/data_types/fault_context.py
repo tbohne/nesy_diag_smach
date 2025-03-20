70aa362ecd5cc3b60709bbd16f7ea5d3e84062fc
tbohne <tim.bohne@dfki.de>
2025-03-20 13:54:50 +0100

    update fault context rep

--- nesy_diag_smach/data_types/fault_context.py
+++ nesy_diag_smach/data_types/fault_context.py
@@ -10,15 +10,15 @@ class FaultContext:
     Represents fault context data, which is communicated to the state machine.
     """
 
-    def __init__(self, error_code_list: List[str], diag_subject_id: str) -> None:
+    def __init__(self, error_code_list: List[str], diag_entity_id: str) -> None:
         """
         Inits the fault context data.
 
         :param error_code_list: list of error codes
-        :param diag_subject_id: identification number for the diagnostic subject
+        :param diag_entity_id: identification number for the diagnostic entity
         """
         self.error_code_list = error_code_list
-        self.diag_subject_id = diag_subject_id
+        self.diag_entity_id = diag_entity_id
 
     def get_json_representation(self) -> Dict[str, str]:
         """
@@ -28,7 +28,7 @@ class FaultContext:
         """
         return {
             "error_code_list": self.error_code_list,
-            "diag_subject_id": self.diag_subject_id
+            "diag_entity_id": self.diag_entity_id
         }
 
     def __str__(self) -> str:
@@ -37,4 +37,4 @@ class FaultContext:
 
         :return: string representation of fault context data
         """
-        return f"error code list: {self.error_code_list},\ndiag subject ID: {self.diag_subject_id}"
+        return f"error code list: {self.error_code_list},\ndiag entity ID: {self.diag_entity_id}"

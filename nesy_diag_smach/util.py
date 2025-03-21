612205c8f81bfa0b39693053771760549cd6e14f
tbohne <tim.bohne@dfki.de>
2025-03-21 16:25:47 +0100

    minor refinements

--- nesy_diag_smach/util.py
+++ nesy_diag_smach/util.py
@@ -46,14 +46,14 @@ def preprocess_time_series_based_on_model_meta_info(
         model_meta_info: Dict[str, str], values: List[float], verbose: bool = True
 ) -> List[float]:
     """
-    Preprocesses the time series based on model metadata (e.g. normalization method).
+    Preprocesses the time series based on model metadata (e.g., normalization method).
     The preprocessing always depends on the trained model that is going to be applied.
     Therefore, this kind of meta information has to be saved for each trained model.
 
-    :param model_meta_info: metadata for the trained model (e.g. normalization method)
+    :param model_meta_info: metadata for the trained model (e.g., normalization method)
     :param values: raw input (time series values)
     :param verbose: whether terminal logs should be enabled
-    :return: preprocessed input (voltage values)
+    :return: preprocessed input
     """
     if verbose:
         print("model meta info:", model_meta_info)
@@ -73,7 +73,7 @@ def no_trained_model_available(sensor_data: SensorData, suggestion_list: Dict[st
     Handles cases where no trained model is available for the specified component.
 
     :param sensor_data: sensor data
-    :param suggestion_list: suspect components suggested for analysis {comp_name: (reason_for, osci_usage)}
+    :param suggestion_list: suspect components suggested for analysis {comp_name: (reason_for, sensor_usage)}
     """
     print("no trained model available for the signal (component) to be classified:", sensor_data.comp_name)
     print("adding it to the list of components to be verified manually..")
@@ -85,7 +85,7 @@ def invalid_model(sensor_data: SensorData, suggestion_list: Dict[str, Tuple[str,
     Handles cases where no valid trained model is available for the specified component.
 
     :param sensor_data: sensor data
-    :param suggestion_list: suspect components suggested for analysis {comp_name: (reason_for, osci_usage)}
+    :param suggestion_list: suspect components suggested for analysis {comp_name: (reason_for, sensor_usage)}
     :param error: thrown value error
     """
     print("invalid model for the signal (component) to be classified:", sensor_data.comp_name)
